1c2d3542d715a9110824640d583684ab07d35cb2
ELIXREIX <gofafafa1@gmail>
2022-12-23 14:15:49 +0700

    update

---
+++ lab3.java
@@ -0,0 +1,19 @@
+public class lab3 {
+    public static void main(String[] args) {
+
+            int mark = 49;         // set the value of mark here! 
+      
+            System.out.println("The mark is " + mark); 
+      
+            if (mark >= 50) { 
+      
+               System.out.println("PASS"); 
+      
+            } else { 
+      
+               System.out.println("FAIL"); 
+            } 
+      
+         } 
+      
+      } 
---
+++ lab32.java
@@ -0,0 +1,18 @@
+public class lab32 {
+    public static void main(String[] args) {
+        int number = 49;       // set the value of number here! 
+
+      System.out.println("The number is " + number); 
+
+      if (number % 2 == 0) { 
+
+         System.out.println("Even number"); 
+
+      } else { 
+
+         System.out.println("odd number"); 
+
+      } 
+
+   } 
+}
