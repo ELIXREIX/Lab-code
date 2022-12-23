5cf740f4f9dd87f890b87cfb7d9a117c1930246e
ELIXREIX <gofafafa1@gmail>
2022-12-23 09:01:10 +0700

    update

---
+++ week2/CentToInch.java
@@ -0,0 +1,9 @@
+public class CentToInch {
+    public static void main(String[] args) {
+        double centvalue = 3.34; 
+        double centtoinch = 0.393701;
+        double inchvalue = centvalue * centtoinch;
+
+        System.out.println(centvalue + " centimeter = " + (inchvalue) + " Inch");
+    }
+}
