cb62e462e03042493ccebbeaa1de71aa7cd6867d
Lucilary <gofafafa1@gmail.com>
2023-06-09 14:07:09 +0700

    update after class

--- Term 2/week1/oopweek1/IS1.java
+++ Term 2/week1/oopweek1/IS1.java
@@ -2,10 +2,20 @@ package oopweek1;
 
 public class IS1 {
     public static void main(String[] args) {
-        students a = new students(871);
+        students a = new students("0158");
         a.getstudentID();
         a.setName("water");
         a.setMajor("CAI");
         a.sayhello();
+        students b = new students("0162");
+        b.getstudentID();
+        b.setName("jack");
+        b.setMajor("DIT");
+        b.sayhello();
+        students c = new students("0175");
+        c.getstudentID();
+        c.setName("Tommy");
+        c.setMajor("RAE");
+        c.sayhello();
     }
 }
