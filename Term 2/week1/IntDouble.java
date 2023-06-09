cbdcd75e25606400d9a3f5e51207267f9909811c
Lucilary <gofafafa1@gmail.com>
2023-06-09 15:20:47 +0700

    update done class

---
+++ Term 2/week1/IntDouble.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IntDouble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your A: ");
+        double A = sc.nextDouble();
+        System.out.print("Enter your B: ");
+        double B = sc.nextDouble();
+        if (A >= B ) {
+          System.out.println((A)+ " " +(B));
+        }
+        else {
+             System.out.println((B)+ " " +(A));
+        }
+        sc.close();
+    }
+}
